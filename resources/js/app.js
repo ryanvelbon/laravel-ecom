052ac5099edc9eda63f060299befa0926ef3dd1e
Ryan <vellabonelloryan@gmail.com>
2021-06-25 16:18:20 +0200

    Bug fix & Add Item from product page
    1. Fixes bug: prior to this commit, multiple 'view cart' buttons were appearing when the same 'Add to Cart' button was clicked more than once. 2. Customer can add item to cart also from products.show page. JS code for 'Add to Cart' btn was moved to app.js so that it can be used across the website.

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,3 +13,29 @@ $(".ajax-form").on("submit", function(event){
 		console.log(data);
 	});
 });
+
+
+
+$(".add-to-cart-form").on("submit", function(event){
+
+	event.preventDefault();
+	var btn = $(this).find('button[type="submit"]');
+	var originalBtnText = btn.html();
+
+	btn.prop('disabled', true);
+	btn.html('adding...');
+
+	var url = $(this).attr("action");
+	var formData= $(this).serialize();
+
+	$.post(url, formData, function(data){
+		console.log(data);
+		btn.prop('disabled', false);
+		btn.html(originalBtnText);
+		if(!btn.hasClass("already-clicked")){
+			$( '<a class="btn btn-outline-primary" href="/cart">View Cart</a>' ).insertAfter(btn);
+		}
+		btn.addClass("already-clicked");
+		$('#nCartItems').html(parseInt($('#nCartItems').html())+1);
+	});
+});
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -10,7 +10,15 @@
 				<h1>{{$product->title}}</h1>
 				<small>{{$product->sku}}</small>
 				<h2>{{$product->price}}</h2>
-				<a href="#" class="btn btn-primary">Add to Cart</a>
+
+				<form class="add-to-cart-form" method="POST" action="{{ route('cart.addItem') }}">
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+					<input type="hidden" name="product_id" value="{{ $product->id }}">
+					<input type="number" name="quantity" value="1">
+					<button type="submit" class="btn btn-primary">Add to Cart</button>
+					<input type="hidden" name="token" value="{{ Session::token() }}">
+				</form>
+
 				<br>
 				<p>{{$product->description}}</p>
 			</div>
